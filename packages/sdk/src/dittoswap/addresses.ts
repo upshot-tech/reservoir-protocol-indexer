3d7bc27d7e6150c9e0be305a6eca16c647a0544b
Diego Campo <xmariachi@gmail.com>
2023-05-15 19:40:24 +0200

    addresses of main contracts

--- packages/sdk/src/dittoswap/addresses.ts
+++ packages/sdk/src/dittoswap/addresses.ts
@@ -1,5 +1,13 @@
 import { ChainIdToAddress, Network } from "../utils";
 
-export const PairFactory: ChainIdToAddress = {
-  [Network.Ethereum]: "TODO",
+export const LpNft: ChainIdToAddress = {
+  [Network.EthereumGoerli]: "0x219a934787e4b73dfc1407365b86750cef116e73",
+};
+
+export const DittoPoolFactory: ChainIdToAddress = {
+  [Network.EthereumGoerli]: "0x3b1954d4193e6f8622a42e7f7c6a2669e538d242",
+};
+
+export const DittoPoolLin: ChainIdToAddress = {
+  [Network.EthereumGoerli]: "0xd3220d10f068886898b887f77aa5b5b4eb6c52af",
 };
