2123cf67c91588228ce3c4c8073ca998ca3358c3
Diego Campo <xmariachi@gmail.com>
2023-05-16 11:01:21 +0200

    new addresses 2023-05-16

--- packages/sdk/src/dittoswap/addresses.ts
+++ packages/sdk/src/dittoswap/addresses.ts
@@ -1,13 +1,13 @@
 import { ChainIdToAddress, Network } from "../utils";
 
 export const LpNft: ChainIdToAddress = {
-  [Network.EthereumGoerli]: "0x219a934787e4b73dfc1407365b86750cef116e73",
+  [Network.EthereumGoerli]: "0x0c5c92f9c93deb468ff496596b89fb12bacb8e28",
 };
 
 export const DittoPoolFactory: ChainIdToAddress = {
-  [Network.EthereumGoerli]: "0x3b1954d4193e6f8622a42e7f7c6a2669e538d242",
+  [Network.EthereumGoerli]: "0xa08cb11c299c3341be9243e511659f31a42be144",
 };
 
 export const DittoPoolLin: ChainIdToAddress = {
-  [Network.EthereumGoerli]: "0xd3220d10f068886898b887f77aa5b5b4eb6c52af",
+  [Network.EthereumGoerli]: "0x11984fbe4c2bb5ebe868c171f527b76e89b74f5b",
 };
