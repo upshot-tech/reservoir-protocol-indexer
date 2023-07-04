2e63fdb9d7202c03ee2c7da859ae6223adaa999e
Sean Matthew <s.matthew.english@gmail.com>
2023-07-04 13:17:06 -0400

    update

--- packages/sdk/src/ditto/addresses.ts
+++ packages/sdk/src/ditto/addresses.ts
@@ -1,8 +1,13 @@
 import { ChainIdToAddress, Network } from "../utils";
 
+export const Pool: ChainIdToAddress = {
+  [Network.Ethereum]: "0x0000000000000000000000000000000000000000",
+  [Network.EthereumGoerli]: "0x2f4776214A9741F3E97E3CB4C0D9Fd75B4777687",
+};
+
 export const PoolFactory: ChainIdToAddress = {
   [Network.Ethereum]: "0x0000000000000000000000000000000000000000",
-  [Network.EthereumGoerli]: "0x1D0527246ACCe4eA1Ff31F6aa3907d4de00784dD",
+  [Network.EthereumGoerli]: "0xF7cd2CD77eEbC441ac31F17d52006Dc1De0eF538",
 };
 
 export const Test721: ChainIdToAddress = {
@@ -14,3 +19,5 @@ export const Test20: ChainIdToAddress = {
   [Network.Ethereum]: "0x0000000000000000000000000000000000000000",
   [Network.EthereumGoerli]: "0x4B3B968a56e0354694cc212D8EbB2c3e8944C15B",
 };
+
+
