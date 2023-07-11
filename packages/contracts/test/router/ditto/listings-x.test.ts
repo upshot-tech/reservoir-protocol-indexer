3fc4a1cabf19e3c38ab7e71ea02c959240d3c32c
Sean Matthew <s.matthew.english@gmail.com>
2023-07-11 14:35:01 -0400

    seems to work

--- packages/contracts/test/router/ditto/listings-x.test.ts
+++ packages/contracts/test/router/ditto/listings-x.test.ts
@@ -9,7 +9,8 @@ import abiErc721 from "../../../../sdk/src/ditto/abis/Erc721.json";
 import abiDittoAppraisal from "../../../../sdk/src/ditto/abis/Appraisal.json";
 import abiDittoPoolFactory from "../../../../sdk/src/ditto/abis/PoolFactory.json";
 import abiUpshotOracle from "../../../../sdk/src/ditto/abis/Oracle.json";
-
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 
 import * as Sdk from "../../../../sdk/src";
 
@@ -34,80 +35,71 @@ describe("DittoModule", () => {
         initialTokenBalance = "10000000000000000000";
     });
 
-    it("Accept multiple listings", async () => {
-
-
-
+    it("x", async () => {
 
         const upshotOracle: Contract = new Contract(
-            "0x777C02961C97FF2FaD78a86ec6a6d759576dcf0c",
+            "0xaa6AEEEd815f8b26bE3318Df37A7F8A9d40C48D7",
             abiUpshotOracle,
             ethers.provider 
         );
 
+        let nonce = await upshotOracle.getNonce("0x3BcEcaE1a61f53Ead737fBd801C9D9873917e5C6"); //
+        console.log("                       nonce", nonce);
+        
+
         const authenticatorAddress00 = await upshotOracle.authenticator();
+        console.log("      authenticatorAddress00", authenticatorAddress00);
 
-        console.log("authenticatorAddress00", authenticatorAddress00);
+        const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+        //const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
+        console.log("              wallet.address", wallet.address);
+       
+        
 
-        const authenticatorSigner: SignerWithAddress = await ethers.getImpersonatedSigner(authenticatorAddress00);
-        await upshotOracle.connect(authenticatorSigner).setAuthenticator(deployer.address);
-
-        const authenticatorAddress01 = await upshotOracle.authenticator();
-
-        console.log("authenticatorAddress01", authenticatorAddress01);
-        console.log("      deployer.address", deployer.address);
-
-
+        let timestamp = 0;
         let messageHash = ethers.utils.solidityKeccak256(
             [
                 'uint256',
-                'bytes',
                 'uint256',
                 'address',
-                'uint96',
-                'address',
-                'uint96',
                 'uint256',
-                'uint256'
+                'address',
+                'uint256',
+                'uint96',
+                'uint96'
             ],
             [
                 5, //block.chainId
-                "0x00",
-                1,
-                "0x3BcEcaE1a61f53Ead737fBd801C9D9873917e5C6",
-                0,
-                "0x607702b48528C2883ADF0A24b8A5e1b5988082d6",
-                "79228162514264337593543950335",
-                4,
-                parseEther("1.1")
-            ]);
-             
+                1, //data.nonce,
+                "0x3BcEcaE1a61f53Ead737fBd801C9D9873917e5C6", //data.nft, 
+                4, //data.nftId, 
+                "0x607702b48528C2883ADF0A24b8A5e1b5988082d6", //data.token, 
+                100, //data.price, 
+                timestamp, //0, //data.timestamp,
+                "79228162514264337593543950335", //data.expiration
+            ]
+        ); 
+        let messageHashBytes = ethers.utils.arrayify(messageHash)
+        let flatSig = await wallet.signMessage(messageHashBytes);
 
-        let signature00 = await deployer.signMessage(ethers.utils.arrayify(messageHash));
-        console.log("signature00: ", signature00);
-        let signature01 = await deployer.signMessage(messageHash);
-        console.log("signature01: ", signature01);
-        console.log("messageHash: ", ethers.utils.arrayify(messageHash));
-        console.log("messageHash: ", messageHash);
-
-        const priceData = {
-            signature: "0x00",
+        const priceData: any = {
+            signature: flatSig,
             nonce: 1,
             nft: "0x3BcEcaE1a61f53Ead737fBd801C9D9873917e5C6",
-            timestamp: 0,
-            token: "0x607702b48528C2883ADF0A24b8A5e1b5988082d6",
-            expiration: "79228162514264337593543950335",
             nftId: 4,
-            price: parseEther("1.1") 
-        }
-        priceData.signature = signature00;
-
+            token: "0x607702b48528C2883ADF0A24b8A5e1b5988082d6",
+            price: 100,
+            timestamp: timestamp, //0,
+            expiration: "79228162514264337593543950335"    
+        };
         
 
-        await upshotOracle.connect(deployer).decodeTokenPrices(
-            [priceData]
-        );
-
+        let output = await upshotOracle.connect(deployer).decodeTokenPrices(priceData);
+        await output.wait();
+        
+        
+        const value00 = await upshotOracle.value00();
+        console.log("                     value00", value00);
   
 
 
