03c762d1c6390ffb6b301dc822e5297de70af522
Tyler <tyler@cyb3r.space>
2023-07-31 19:48:29 -0400

    refactor: get listings test to work after renaming to ditto

--- packages/sdk/src/ditto/addresses.ts
+++ packages/sdk/src/ditto/addresses.ts
@@ -19,3 +19,18 @@ export const DittoPoolRouterRoyalties: ChainIdToAddress = {
   [Network.Ethereum]: "TBD",
   [Network.EthereumGoerli]: "0x9D561aFFF27270c5c226C78EaB963ea7F6D37550",
 };
+
+export const Test721: ChainIdToAddress = {
+  [Network.Ethereum]: "0x0000000000000000000000000000000000000000",
+  [Network.EthereumGoerli]: "0x0133B5f5601D0B2980ac812A1719760ba3ea53e7",
+};
+
+export const Test20: ChainIdToAddress = {
+  [Network.Ethereum]: "0x0000000000000000000000000000000000000000",
+  [Network.EthereumGoerli]: "0x8cAa8de40048C4c840014BdEc44373548b61568d",
+};
+
+export const UpshotOracle: ChainIdToAddress = {
+  [Network.Ethereum]: "0x0000000000000000000000000000000000000000",
+  [Network.EthereumGoerli]: "0x24D3906675C35590B95c9Cc393ab11619624e840",
+};
